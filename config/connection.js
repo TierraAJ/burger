3971bb5d9a8c61f917a310a5879df82ad63f66a6
Tierra Austin-Johnson <38711594+TbabyAJ@users.noreply.github.com>
2018-11-08 17:17:18 -0500

    added bkgd img, navbar, and jumbotron

--- config/connection.js
+++ config/connection.js
@@ -3,25 +3,17 @@ var mysql = require("mysql");
 
 var connection;
 
-// connection= mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "",
-//   password: "",
-//   database: "krusty_krab_db"
-// });
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
   else {
-    connection = mysql.createConnection({
-      host: "enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      port: 3306,
-      user: "g31yha3y4y6rh7it",
-      password: "absdcgcn00h0vlet",
-      database: "a17pz637jmnud92k"
-    });
+    connection= mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "",
+    password: "",
+    database: "krusty_krab_db"
+});
   };
 // Make connection.
 connection.connect(function(err) {
